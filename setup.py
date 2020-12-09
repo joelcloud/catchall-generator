6c007cfacf9eb90d018f66f5d1b77944c26ad893
Joel Nyberg <carljoelnyberg@gmail.com>
2020-12-09 12:39:38 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
     name='catchall-generator',
-    version='1.0',
+    version='1.1',
     author='joelcloud',
     license='MIT',
     description='Generate emails for your catchall adress.',
     long_description=long_description,
     url='https://github.com/joelcloud/catchall-generator',
-    packages=find_packages(),
+    packages=['catchall_generator'],
 )
